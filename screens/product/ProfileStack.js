61147bc5aa7b4b714b3ffab04be9672a59f9f26d
luanlbps16836 <luanlbps16836@fpt.edu.vn>
2022-11-15 16:18:07 +0700

    userProfile SemiFinal

--- screens/product/ProfileStack.js
+++ screens/product/ProfileStack.js
@@ -1,56 +1,78 @@
-
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { TouchableOpacity ,Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 const ProfileStack = () => {
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
             <View style={styles.container}>
                 <View style={styles.user}>
-                    <Image style={styles.userImg} source={require('../../assets/images/userImg1.jpg')}></Image>
+                    <Image style={styles.userImg} source={require('../testProject/assets/img/userImg1.png')}></Image>
                     <View style={styles.userInfo}>
-                        <Text style={styles.userName}>Le Ba Luan</Text>
-                        <Text style={styles.userMoney}>$100.00</Text>
-                        <TouchableOpacity style={styles.userRecharge}>Recharge</TouchableOpacity>
+                        <View>
+                          <Text style={styles.userName}>Le Ba Luan</Text>
+                        </View>
+                        <View>
+                          <Text style={styles.userMoney}>$100.00</Text>
+                        </View>
+                        <TouchableOpacity style={styles.userRecharge}>
+                          <Text style={styles.userRechargeText}>Recharge</Text> 
+                        </TouchableOpacity>
                     </View>
                 </View>
                 <View style={styles.funtion}>
                     <View style={styles.language}>
-                        <Image style={styles.languageIcon} source={require('')}></Image>
-                        <Text style={styles.languageText}>Language</Text>
-                        <select value={selects} style={styles.languageSelect}>
+                        <Image style={styles.languageIcon} source={require('../testProject/assets/img/internet.png')}></Image>
+                        <View>
+                          <Text style={styles.languageText}>Language</Text>
+                        </View>
+                        {/* <select value={selects} style={styles.languageSelect}>
                             <option value={selectsEN}>EN</option>
                             <option value={selectsVN}>VN</option>
-                        </select>
+                        </select> */}
                     </View>
                     <View style={styles.mode}>
-                        <Image style={styles.modeIcon} source={require('')}></Image>
-                        <Text style={styles.modeText}>Mode</Text>
-                        <Image style={styles.modeSwitch} source={require('')}></Image>
+                        <Image style={styles.modeIcon} source={require('../testProject/assets/img/dark-mode.png')}></Image>
+                        <View>
+                          <Text style={styles.modeText}>Mode</Text>
+                        </View>
+                        <Image style={styles.modeSwitch} ></Image>
                     </View>
                     <View style={styles.notifications}>
-                        <Image style={styles.notificationsIcon} source={require('')}></Image>
-                        <Text style={styles.notificationsText}>Notifications</Text>
-                        <Image style={styles.notificationsSwitch} source={require('')}></Image>
+                        <Image style={styles.notificationsIcon} source={require('../testProject/assets/img/notification.png')}></Image>
+                        <View>
+                          <Text style={styles.notificationsText}>Notifications</Text>
+                        </View>
+                        <Image style={styles.notificationsSwitch} ></Image>
                     </View>
                     <View style={styles.favorite}>
-                        <Image style={styles.favoriteIcon} source={require('')}></Image>
-                        <Text style={styles.favoriteText}>Favorite</Text>
+                        <Image style={styles.favoriteIcon} source={require('../testProject/assets/img/lover.png')}></Image>
+                        <View>
+                          <Text style={styles.favoriteText}>Favorite</Text>
+                        </View>
                     </View>
                     <View style={styles.history}>
-                        <Image style={styles.historyIcon} source={require('')}></Image>
-                        <Text style={styles.historyText}>History</Text>
+                        <Image style={styles.historyIcon} source={require('../testProject/assets/img/history.png')}></Image>
+                        <View>
+                          <Text style={styles.historyText}>History</Text>
+                        </View>
                     </View>
                     <View style={styles.changePassword}>
-                        <Image style={styles.changePasswordIcon} source={require('')}></Image>
-                        <Text style={styles.changePasswordText}>Change Password</Text>
+                        <Image style={styles.changePasswordIcon} source={require('../testProject/assets/img/password.png')}></Image>
+                        <View>
+                          <Text style={styles.changePasswordText}>Change Password</Text>
+                        </View>
                     </View>
                     <View style={styles.support}>
-                        <Image style={styles.supportIcon} source={require('')}></Image>
-                        <Text style={styles.supportText}>Support</Text>
+                        <Image style={styles.supportIcon} source={require('../testProject/assets/img/support.png')}></Image>
+                        <View>
+                          <Text style={styles.supportText}>Support</Text>
+                        </View>
                     </View>
                     <View style={styles.logout}>
-                        <Image style={styles.logoutIcon} source={require('')}></Image>
-                        <Text style={styles.logoutText}>Logout</Text>
+                        <Image style={styles.logoutIcon} source={require('../testProject/assets/img/logout.png')}></Image>
+                        <View>
+                          <Text style={styles.logoutText}>Logout</Text>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -62,187 +84,162 @@ export default ProfileStack
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        padding: 20
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      padding: 20
     },
     user: {
-        fontSize: 20,
-        color: '#333333'
+      height: 142,
+      width: '100%',
+      flexDirection: 'row',
+      marginTop: 48,
+      //backgroundColor: 'red'
     },
     userImg: {
-        height: 150,
-        width: 150,
-        borderRadius: 75
+      height: 142,
+      width: 142,
+      borderRadius: 75
     },
     userInfo: {
-        fontSize: 12,
-        fontWeight: '600',
-        color: '#666',
-        textAlign: 'center',
-        marginBottom: 10
+      height: '100%',
+      width: '100%',
+      flexDirection: 'column',
+      paddingLeft: 80,
+      justifyContent: 'center',
     },
     userName: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginTop: 10,
-        marginBottom: 10
+      fontSize: 24,
     },
     userMoney: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        width: '100%',
-        marginBottom: 10
+      fontSize: 20,
+      paddingBottom: 28,
+      padding: 14
     },
     userRecharge: {
-        borderColor: '#2e64e5',
-        borderWidth: 2,
-        borderRadius: 3,
-        paddingVertical: 8,
-        paddingHorizontal: 12,
-        marginHorizontal: 5
+        backgroundColor: 'orange',
+        borderRadius: 28,
+        height: 47,
+        width: 127,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    userRechargeText: {
+      fontSize: 20,
     },
     funtion: {
-        color: '#2e64e5'
+      color: '#fff',
+      width: '100%',
+      marginTop: 31,
+      //backgroundColor: 'blue'
     },
     language: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     languageIcon: {
-        justifyContent: 'center'
+      
     },
     languageText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
     languageSelect: {
-        fontSize: 12,
-        color: '#666',
-        textAlign: 'center'
+      
     },
     mode: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     modeIcon: {
-        justifyContent: 'center'
+  
     },
     modeText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
     modeSwitch: {
-        fontSize: 12,
-        color: '#666',
-        textAlign: 'center'
+      
     },
     notifications: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     notificationsIcon: {
-        justifyContent: 'center'
+      
     },
     notificationsText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
     notificationsSwitch: {
-        fontSize: 12,
-        color: '#666',
-        textAlign: 'center'
+      
     },
     favorite: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     favoriteIcon: {
-        justifyContent: 'center'
+      
     },
     favoriteText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
     history: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     historyIcon: {
-        justifyContent: 'center'
+      
     },
     historyText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
     changePassword: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     changePasswordIcon: {
-        justifyContent: 'center'
+      
     },
     changePasswordText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
     support: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     supportIcon: {
-        justifyContent: 'center'
+      
     },
     supportText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
     logout: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginVertical: 20
+      flexDirection: 'row',
+      padding: 25,
+      left: -25
     },
     logoutIcon: {
-        justifyContent: 'center'
+      
     },
     logoutText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        textAlign: 'center'
+      fontSize: 20,
+      paddingLeft: 31
     },
-})
+  })
 
 
 
